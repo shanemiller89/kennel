a346f5229276859b4781c0fcc52ae1e0f78b05a5
Shane Miller <shanemiller89@gmail.com>
2019-07-11 17:19:18 -0500

    practice completed

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -5,6 +5,7 @@ export default class EmployeeList  extends Component {
     render() {
         return (
             <section className="employees">
+                <h1>Employee List</h1>
             {
                 this.props.employees.map(employee =>
                     <div key={employee.id}>
