cc6100310fdad493c96f2b3393e5838b6c6e5c41
Shane Miller <shanemiller89@gmail.com>
2019-07-15 12:41:28 -0500

    intial start

---
+++ src/components/search/SearchResults.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class SearchResults extends Component {
+    render() {
+        return (
+            <section className="searchResults">
+
+            </section>
+        )
+    }
+}
